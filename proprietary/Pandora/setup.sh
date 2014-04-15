2126ad81dbafc6886f58bcae1cf332ed5aae374c
Michael Eden <themichaeleden@gmail.com>
2014-04-15 14:48:01 -0500

    Added Pandora support

---
+++ proprietary/Pandora/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo pacman -S pianobar
+
+echo 'default_driver=pulse' >/etc/libao.conf
+
+mkdir ~/.config/pianobar
+
+cat secret/pianobar.conf > ~/.config/pianobar/config
+
+mkfifo ~/.config/pianobar/ctl
