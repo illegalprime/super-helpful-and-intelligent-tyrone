8ef1b8de29e0794a05522ce1e4381ca5ec0c5591
Michael Eden <themichaeleden@gmail.com>
2015-05-02 18:06:36 +0000

    Changed video memory to 128MB for Kodi (XBMC)

--- kodi/setup.sh
+++ kodi/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-pacman -Sy xorg-xinit xorg-server xf86-video-fbturbo-git kodi-rbp
+sudo pacman -Sy xorg-xinit xorg-server xf86-video-fbturbo-git kodi-rbp
+
+sudo sed -i.backup s/gpu_mem=[0-9]*/gpu_mem=128/g /boot/config.txt
 
